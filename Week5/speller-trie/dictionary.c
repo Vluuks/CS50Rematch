bf6a6a5d008f058b51abd9025ba9324c3a647653
Vluuks <vluuks@gmail.com>
2018-09-30 22:57:22 +0200

    trie check sort of works

--- Week5/speller-trie/dictionary.c
+++ Week5/speller-trie/dictionary.c
@@ -38,21 +38,55 @@ node;
 node* root;
 unsigned int dict_size = 0;
 
-// Returns true if word is in dictionary else false
+/*
+    Iterates over the letters in the word and checks for each letter
+    if it also exists in the trie. If it's the last letter of the word,
+    check if the trie also says that it's actually a word.
+*/
 bool check(const char *word) {
 
-    printf("%s", word);
-    // // if you use current->next then if there is only one
-    // // or it is the last it will never go inside
-    // while(current != NULL) {
+    printf("Checking: %s\n", word);
 
-    //     if(strcasecmp(word, current->word) == 0) {
-    //         return true;
-    //     }
+    // iterate over the letters in the word, starting at root
+    node* cursor = root;
+    for(int i = 0; i < strlen(word); i++) {
 
-    //     // move on to the next node
-    //     current = current->next;
-    // }
+        // get index for letter
+        char c = tolower(word[i]);
+        int index = c - 'a';
+
+        printf("Checking: %c\n", c);
+
+        // check child of cursor for that letter
+        cursor = cursor->children[index];
+
+        // if particular child exists and it is also the last letter
+        if(cursor != NULL) {
+
+            if(i == strlen(word) - 1) {
+                // return cursor->is_word;
+
+                if(cursor->is_word){
+                    printf("Correct! %s\n", word);
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+
+        }
+
+        // this can perhaps be removed later on but now here for understanding
+        // if it does not even exist, it can never be right
+        else {
+            printf("FALSE: %c does not exist as child.\n", word[i]);
+            return false;
+        }
+
+    }
+
+
 
     return false;
 }
@@ -76,7 +110,7 @@ bool load(const char *dictionary) {
     char new_word[LENGTH + 1];
     while(fscanf(infile, "%s", new_word) != EOF) {
 
-        printf("%s", new_word);
+        // printf("%s", new_word);
 
         // get reference in the root
         node* current = root;
@@ -85,14 +119,14 @@ bool load(const char *dictionary) {
         // iterate over letters in word
         for(int i = 0; i < strlen(new_word); i++) {
 
-            printf("%c\n", new_word[i]);
+            // printf("%c\n", new_word[i]);
 
             // if the node at that location does not exist yet
             // (meaning we haven't constructed a word in this fashion)
             int index = tolower(new_word[i]) - 'a';
             if(current->children[index] == NULL) {
 
-                printf("making new node\n");
+                // printf("making new node\n");
 
                 // allocate memory for a new node
                 // at the current location in the trie
@@ -110,6 +144,7 @@ bool load(const char *dictionary) {
 
             // set it to true if last character
             if(i == strlen(new_word) - 1) {
+                printf("Last char, setting true: %c . \n", new_word[i]);
                 temp->is_word = true;
             }
 
@@ -180,6 +215,6 @@ void testeroni() {
             }
         }
 
-        printf("------- next word\n");
+        printf("-------\n");
     }
 }
